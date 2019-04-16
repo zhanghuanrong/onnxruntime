97bdc582937dde87e2df27322893b42ad424a6f7
Lei Zhang <zhang.huanning@hotmail.com>
2019-04-16 15:43:53 -0700

    Formatting text files.

--- onnxruntime/core/providers/cpu/math/element_wise_ops.cc
+++ onnxruntime/core/providers/cpu/math/element_wise_ops.cc
@@ -1038,7 +1038,7 @@ Status Erf<float>::Compute(OpKernelContext* context) const {
   auto& X = *X_ptr;
   auto& Y = *context->Output(0, X.Shape());
 
-  MlasComputeErf(X.template Data<float>(),  Y.template MutableData<float>(),  X.Shape().Size());
+  MlasComputeErf(X.template Data<float>(), Y.template MutableData<float>(), X.Shape().Size());
 
   return Status::OK();
 }
